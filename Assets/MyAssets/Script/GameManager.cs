06ee9f9ce56f1071170a5af39a191291851096c1
aegydm <aaaaydm3148@gmail.com>
2024-06-11 03:35:01 +0900

    로딩화면 완성
    다음 수정 필요.
    - 게임 매니저 버튼 인식 장애
    - 게임 매니저 Ui 매니저 분리
    - 시작 화면 구성 필요

--- Assets/MyAssets/Script/GameManager.cs
+++ Assets/MyAssets/Script/GameManager.cs
@@ -5,30 +5,141 @@ using UnityEditor;
 using UnityEngine.SceneManagement;
 using UnityEditor.SceneManagement;
 
+
+public enum GameState
+{
+    menu,
+    inGame,
+    gameOver
+}
+
+
 public class GameManager : MonoBehaviour
 {
-    public static GameManager GM_Instance;
-    public bool GameOver = false;
+    public static GameManager Instance;
+    //public PlayerInfo playerInfo;
+    public GameState currentGameState = GameState.menu;
+    public float time;
+
+    public GameObject menuUi;
+    public GameObject overUi;
+    public GameObject stopUi;
 
     public string scenename;
-    // Start is called before the first frame update
-    void Start()
+
+    private void Awake()
     {
-        if (GM_Instance == null)
+        if (Instance == null)
         {
-            GM_Instance = this;
+            Instance = this;    //싱글 톤 선언: 이로써 모든 불러오기 허가
             DontDestroyOnLoad(gameObject);
         }
+        else
+        {
+            return;
+        }
+        One("testScenes");
+    }
+    void Start()
+    {
+        Menu();
+        //One("testScenes");
+        //playerInfo = GameObject.Find("Player").GetComponent<PlayerInfo>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        time += Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.Escape)) { StopGame(); }
     }
-    public void One()
+    public void One(string scen)
     {
-        scenename = "MineOne";
-        SceneManager.LoadScene("Loading");
+        scenename = (scen);
+        SceneManager.LoadScene("Loading Scene");
     }
+    void SetGameState(GameState newGameState)
+    {
+        if (newGameState == GameState.menu)
+        {
+            //Menu();                     제귀함수오류발생 방지
+            overUi.SetActive(false);
+            //menuUi.SetActive(true);
+            stopUi.SetActive(false);
+        }
+        else if (newGameState == GameState.inGame)
+        {
+            Ingame();
+            overUi.SetActive(false);
+            menuUi.SetActive(false);
+            stopUi.SetActive(false);
+        }
+        else if (newGameState == GameState.gameOver)
+        {
+            //GameOver();;                제귀함수오류발생 방지
+            overUi.SetActive(true);
+            menuUi.SetActive(false);
+            stopUi.SetActive(false);
+        }
+        currentGameState = newGameState;  //게임상태 검색에 사용하면 되는 변수
+    }
+    //----------------------상태 입력에 따른 값들---------------------------
+    public void Menu()                          //게임 실행시 첫 화면, 시작 버튼이 존재
+    {
+        
+        Time.timeScale = 1;
+        SetGameState(GameState.menu);
+    }
+
+    public void Ingame()                        //게임중인상태 - 노멀상태 일반 몹, 정예몹
+    {
+        Debug.Log("인게임 실행됨");
+        //enemyspawnmanager.pullingenemys();      //에너미 풀링
+        //enemyspawnmanager.stage1();             //스테이지 실행 = 에너미s 4마리 출현 
+    }
+
+    public void GameOver()                      //임시 게임오버 코드 - 다른코드에서 GameManager.instance.GameOver(); 로 호출됨
+    {
+        //if (playerinfo._playerhp <= 0)
+        //{
+        //    time.timescale = 0;
+        //    setgamestate(gamestate.gameover);
+        //}
+        Time.timeScale = 0;
+    }
+    //----------------------버튼 입력에 따른 값들---------------------------
+
+    public void StartGame()        //게임시작 버튼 눌리면 실행
+    {
+        Time.timeScale = 1;
+        SetGameState(GameState.inGame);
+    }
+    public void QuitGame()         //게임 나가기버튼
+    {
+        Application.Quit();
+    }
+
+    public void StopGame()         // 일시정지 (ESC)
+    {
+        if (currentGameState == GameState.inGame)
+        {
+            if (Time.timeScale == 0)
+            {
+                stopUi.SetActive(false);
+                Time.timeScale = 1;
+            }
+            else if (Time.timeScale == 1)
+            {
+                stopUi.SetActive(true);
+                Time.timeScale = 0;
+            }
+        }
+    }
+
+    public void ResetGame()        //다시하기
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    //--------------------------------------------------------------------
+
 }
--- Assets/MyAssets/Script/Loading.cs
+++ Assets/MyAssets/Script/Loading.cs
@@ -10,17 +10,17 @@ using UnityEditor.SceneManagement;
 public class Loading : MonoBehaviour
 {
     [SerializeField] Slider Loading_bar;
-    [SerializeField] float time;
+    [SerializeField] float time; 
     [SerializeField] string scenenames;
     [SerializeField] GameObject Loading_bar_text;
+
     // Start is called before the first frame update
     void Start()
     {
-        scenenames = GameManager.GM_Instance.scenename;
+        scenenames = GameManager.Instance.scenename;
         Loading_bar.maxValue = 1.0f;
         StartCoroutine(LoadScene(scenenames));
     }
-
     // Update is called once per frame
     void Update()
     {
@@ -35,7 +35,9 @@ public class Loading : MonoBehaviour
         yield return null;
         AsyncOperation op = SceneManager.LoadSceneAsync(name); // 비동기 Scene 로딩 ( 로딩할 Scene 이름 )
         op.allowSceneActivation = false;  // Scene 이 로딩 되었을때 바로 실행할지 .
-        yield return new WaitForSecondsRealtime(1.0f); // 1초 대기
+        //yield return new WaitForSecondsRealtime(0.9f); // 1초 대기
+        yield return new WaitForSecondsRealtime(op.progress + 0.9f); // 실제 로딩 대기 + 1초 대기
         op.allowSceneActivation = true; // 로딩된 Scene 실행.
     }
+
 }
